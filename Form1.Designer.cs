6ab940e7f0a3004819b69b71b00c8fbb5bb42354
ark-l <ark-l@ukr.net>
2024-11-06 22:23:24 +0200

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace lab4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbtext = new System.Windows.Forms.TextBox();
+            this.tbres = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnMD5 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbtext
+            // 
+            this.tbtext.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tbtext.Location = new System.Drawing.Point(92, 31);
+            this.tbtext.Name = "tbtext";
+            this.tbtext.Size = new System.Drawing.Size(383, 27);
+            this.tbtext.TabIndex = 0;
+            this.tbtext.Text = "hello";
+            // 
+            // tbres
+            // 
+            this.tbres.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tbres.Location = new System.Drawing.Point(92, 76);
+            this.tbres.Name = "tbres";
+            this.tbres.Size = new System.Drawing.Size(383, 27);
+            this.tbres.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(30, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "text =";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(15, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "result =";
+            // 
+            // btnMD5
+            // 
+            this.btnMD5.Location = new System.Drawing.Point(501, 53);
+            this.btnMD5.Name = "btnMD5";
+            this.btnMD5.Size = new System.Drawing.Size(101, 46);
+            this.btnMD5.TabIndex = 4;
+            this.btnMD5.Text = "MD5";
+            this.btnMD5.UseVisualStyleBackColor = true;
+            this.btnMD5.Click += new System.EventHandler(this.btnMD5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(964, 599);
+            this.Controls.Add(this.btnMD5);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbres);
+            this.Controls.Add(this.tbtext);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbtext;
+        private System.Windows.Forms.TextBox tbres;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnMD5;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace lab4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        void MD5()
+        {
+            byte[] text = Encoding.UTF8.GetBytes(tbtext.Text);
+            List<byte> list;
+            list = text.ToList();
+            if(text.Length % 64 != 0)
+            {
+                list.Add(0x80);
+                if(list.Count %64 != 0)
+                {
+                    do
+                    {
+                        list.Add(0);
+                    } while (list.Count % 64 != 0);
+                }
+                text = list.ToArray();
+            }
+            
+            uint A = 0x67452301;
+            uint B = 0xefcdab89;
+            uint C = 0x98badcfe;
+            uint D = 0x10325476;
+            for(int i = 0; i < text.Length; i += 64)
+            {
+                uint[] X = new uint[16];
+                for(int j = 0; j < 16; j++)
+                {
+                    X[j] = BitConverter.ToUInt32(text, i * 64 + j * 4);
+                }
+                uint A1 = A, B1 = B, C1 = C, D1 = D;
+                for (int j = 0; j < 64; j++)
+                {
+                    A = B + ((A + G(B, C, D, j) + X[g(j)] + T(j)) << S(i));
+                    uint t = D;
+                    D = C; ;
+                    C = A;
+                    B = t;
+                }
+                A = A + A1;
+                B = B + B1;
+                C = C + C1;
+                D = D + D1;
+            }
+            byte[] a = BitConverter.GetBytes(A);
+            byte[] b = BitConverter.GetBytes(B);
+            byte[] c = BitConverter.GetBytes(C);
+            byte[] d = BitConverter.GetBytes(D);
+            List<byte[]> W = new List<byte[]>();
+            W.Add(a);
+            W.Add(b);
+            W.Add(c);
+            W.Add(d);
+            tbres.Text = "";
+            for(int i =0;i < 4; i++)
+            {
+                for (int j = 0; j < a.Length; j++)
+                {
+                    tbres.Text += W[i][j].ToString("x2");
+                }
+            }
+            File.WriteAllText("E:\\Е\\Криптология\\lab4\\1.txt", tbres.Text);
+        }
+        uint T(int i)
+        {
+            uint t = (uint)(Math.Pow(2, 32) * Math.Abs(Math.Sin(i + 1)));
+            return t;
+        }
+        int g(int i)
+        {
+            int g1 = 0;
+            if (i < 16)
+            {
+                g1 = i;
+            }
+            else if (i > 15 && i < 32)
+            {
+                g1 = (5 * i + 1) % 16;
+            }
+            else if (i > 31 && i < 48)
+            {
+                g1 = (3 * i + 5) % 16;
+            }
+            else if (i > 47 && i < 64)
+            {
+                g1 = (7 * i) % 16;
+            }
+            return g1;
+        }
+        uint G(uint b,uint c,uint d, int i)
+        {
+            uint g = 0;
+            if(i < 16)
+            {
+                g = (b & c) | (~b & d);
+            }
+            else if(i > 15 && i < 32)
+            {
+                g = (b & d) | (c & ~d);
+            }
+            else if (i > 31 && i < 48)
+            {
+                g = b ^ c ^ d;
+            }
+            else if (i > 47 && i < 64)
+            {
+                g = c ^ (b | ~d);
+            }
+            return g;
+        }
+        int S(int i)
+        {
+            int[] s = new int[] { 7, 12, 17, 22, 7, 12, 17, 22, 7, 12, 17, 22, 7, 12, 17, 22 };
+            return s[i % 16];
+        }
+        private void btnMD5_Click(object sender, EventArgs e)
+        {
+            MD5();
+        }
+    }
+}
